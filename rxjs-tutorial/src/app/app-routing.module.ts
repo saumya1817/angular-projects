45a246c3f5c20e9cbda63d74268017fb33adfb57
Saumya Singh <saumya.singh1817@gmail.com>
2024-06-30 21:42:29 +0530

    bootstrap header setup

--- rxjs-tutorial/src/app/app-routing.module.ts
+++ rxjs-tutorial/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './component/header/header.component';
+import { ObservablesComponent } from './component/observables/observables.component';
+import { PromisesComponent } from './component/promises/promises.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component: HeaderComponent,
+    path: 'home'
+  },
+  {
+    component: ObservablesComponent,
+    path: 'observables'
+  },
+  {
+    component: PromisesComponent,
+    path: 'promises'
+  },
+  {
+    path: '**',
+    redirectTo: 'promises'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- rxjs-tutorial/src/app/app.module.ts
+++ rxjs-tutorial/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { ObservablesComponent } from './component/observables/observables.component';
+import { PromisesComponent } from './component/promises/promises.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ObservablesComponent,
+    PromisesComponent
   ],
   imports: [
     BrowserModule,
