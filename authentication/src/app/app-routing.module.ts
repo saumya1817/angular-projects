c58f7eb5e7941fcfd5c8e3c3db568e308b909dd7
Saumya <saumya.singh1702@gmail.com>
2025-01-02 23:14:09 -0500

    added authgaurd for dashboard

--- authentication/src/app/app-routing.module.ts
+++ authentication/src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AuthGuard } from './gaurd/auth.guard';
 
 const routes: Routes = [
   { path: '', component: LoginComponent, pathMatch: 'full'},
   { path: 'login', component: LoginComponent},
-  { path: 'dashboard', component: DashboardComponent}
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
