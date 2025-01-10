8772c74452a30f1db756f97d18ee3fa0538ed0b9
Saumya <saumya.singh1702@gmail.com>
2025-01-10 15:33:39 -0500

    Initial setup of tailwind css

---
+++ personality-test/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    content: [
+      './src/**/*.{html,ts}', // Scans all HTML and TypeScript files in the src folder
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }  
