1211018565f775b74657a249689bac166394bc26
Saumya <saumya.singh1702@gmail.com>
2025-01-17 13:21:24 -0500

    Basic testcases using Cypress

---
+++ cypress/e2e/data-communication/sample-tests.cy.js
@@ -0,0 +1,26 @@
+/// <reference types="cypress" />
+describe('Check Page Load', ()=>{
+    beforeEach(()=>{
+        cy.visit('http://localhost:4200/')
+    });
+
+    it('displays heading', ()=>{
+        cy.get('.container h1').should('contain.text', 'Data Communication Using Input Output');
+    });
+
+    it('displays input text box', ()=>{
+        cy.get('.container .parentDiv .parentInput').should('exist');
+    });
+
+    it('displays button to load child', ()=>{
+        cy.get('.container .parentDiv .parentSubmit').should('exist');
+    });
+
+    it('check button text', ()=>{
+        cy.get('.container .parentDiv .parentSubmit').should('contain.value', 'Load Child Component');
+    });
+
+    it('click on button', ()=>{
+        cy.get('.container .parentDiv .parentSubmit').click()
+    })
+});
